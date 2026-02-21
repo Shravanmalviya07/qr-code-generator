e508a18678cd1ad2544b1e625f6b5420dff38761
Shravanmalviya07 <sd7939749@gmail.com>
2026-02-21 22:18:07 +0530

    Add files via upload

---
+++ QRcode/generate_qrcode.py
@@ -0,0 +1,18 @@
+import g_rcode
+from g_rcode import QRCode
+from qrcode.constants import ERROR_CORRECT_L
+
+qr = QRCode(
+    version=1,
+    error_correction=ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+qr.add_data("put your text here")
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("custom_qrcode.png")
+
+print("Custom QR Code Generated!")
